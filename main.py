d49681d7f41b0162d02275543ac74afc4d5e6c93
LiuZiRui764 <3470924079@qq.com>
2024-08-18 11:15:05 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import streamlit as st
-from util import generate_script
+from utils import generate_script
 
-st.title("📽 Video Script Generator")
+st.title("🎬 视频脚本生成器")
 
 with st.sidebar:
-    openai_api_key = st.text_input("Please input your OpenaiAPI",type="password")
-    st.write("""Methods of getting API keys:
-             'https://api.aigc369.com/register?aff=87kh'""")
-subject = st.text_input("💡 Topic of the video")
-video_length = st.number_input("⏰ Video Duration(min)",min_value=0.1,step = 0.5)
-creativity = st.slider("""🕹 Video creativity 
-                        (the smaller the creativity, the more rigorous the result; the higher the creativity, the more flexible the result)""",
-                       min_value=0.0,max_value=1.5,step=0.1,value=0.2)
-submit = st.button("generate script")
+    openai_api_key = st.text_input("请输入OpenAI API密钥：", type="password")
+    st.markdown("[获取OpenAI API密钥](https://platform.openai.com/account/api-keys)")
+
+subject = st.text_input("💡 请输入视频的主题")
+video_length = st.number_input("⏱️ 请输入视频的大致时长（单位：分钟）", min_value=0.1, step=0.1)
+creativity = st.slider("✨ 请输入视频脚本的创造力（数字小说明更严谨，数字大说明更多样）", min_value=0.0,
+                       max_value=1.0, value=0.2, step=0.1)
+submit = st.button("生成脚本")
+
 if submit and not openai_api_key:
-    st.write("🔒 Warning : Please input your openai key.")
+    st.info("请输入你的OpenAI API密钥")
     st.stop()
 if submit and not subject:
-    st.write("🔒 Warning : Please input your subject.")
+    st.info("请输入视频的主题")
     st.stop()
-if submit and video_length < 0.1:
-    st.write("🔒 Warning : The video duration should be more than 0.1 minutes.")
+if submit and not video_length >= 0.1:
+    st.info("视频长度需要大于或等于0.1")
     st.stop()
 if submit:
-    with st.spinner(("Loading...")):
-        title,script = generate_script(subject,video_length,creativity,openai_api_key)
-    st.write("Loading completed")
-    st.subheader("✍ Title:")
+    with st.spinner("AI正在思考中，请稍等..."):
+        search_result, title, script = generate_script(subject, video_length, creativity, openai_api_key)
+    st.success("视频脚本已生成！")
+    st.subheader("🔥 标题：")
     st.write(title)
-    st.subheader("📜 Script:")
+    st.subheader("📝 视频脚本：")
     st.write(script)
+    with st.expander("维基百科搜索结果 👀"):
+        st.info(search_result)
